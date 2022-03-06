6639909abc08221c0917dccdd34a1581a3481624
zhengzemeng1999@163.com <zhengzemeng1999@163.com>
2022-03-06 18:00:52 +0800

    modified lua file

--- premake5.lua
+++ premake5.lua
@@ -25,11 +25,7 @@ project "GLFW"
     }
 
     filter "system:windows"
-
         systemversion "latest"
-        buildoptions {"-std=c11", "-lgdi32"}
-        systemversion "latest"
-        staticruntime "On"
     
     files
     {
